9328bc5c93ef1db328abb9c26b976cfedd6c28f4
Murad <muradteymurov2000@gmail.com>
2022-05-31 22:01:31 +0400

    listpagecommit

--- bootcamp m4 new/src/pages/ListPage/ListPage.js
+++ bootcamp m4 new/src/pages/ListPage/ListPage.js
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
+import { initialState, reducer } from '../../cases';
 import './ListPage.css';
 
 class ListPage extends Component {
     state = {
-        movies: [
-            { title: 'The Godfather', year: 1972, imdbID: 'tt0068646' }
-        ]
+        title: '',
+        movies: []
+
     }
     componentDidMount() {
-        const id = this.props.match.params;
-        console.log(id);
-        // TODO: запрос к сервер на получение списка
-        // TODO: запросы к серверу по всем imdbID
+        const id = this.props.match.params.id;
+        if (id) {
+            fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+                .then(resp => {
+                    if (resp.ok) {
+                        return resp.json();
+                    } else {
+                        alert('No list')
+                    };
+                })
+                .then(data => {
+                    console.log(data);
+                    if (data) {
+                        let movie = []
+                        data.movies.map(elem => {
+                            fetch(`http://www.omdbapi.com/?i=${elem}&apikey=53638081`)
+                                .then(res => res.json())
+                                .then(data => { movie.push({ id: elem, name: data.Title }); 
+                                this.setState({ movies: movie, title: data.title }) })
+                        })
+                    } else {
+                        alert('No list')
+                    }
+                })
+        }
+
     }
-    render() { 
+
+    render() {
+
         return (
             <div className="list-page">
-                <h1 className="list-page__title">Мой список</h1>
+                <h1 className="list-page__title">{this.state.title}</h1>
                 <ul>
                     {this.state.movies.map((item) => {
                         return (
-                            <li key={item.imdbID}>
-                                <a href="https://www.imdb.com/title/tt0068646/" >{item.title} ({item.year})</a>
+                            <li key={item.id}>
+
+                                <a href={`https://www.imdb.com/title/${item.id}/`} target="_blank">{item.name}</a>
+
                             </li>
-                        );
+                        )
                     })}
                 </ul>
-            </div>
+            </div >
         );
     }
 }
- 
 export default ListPage;
